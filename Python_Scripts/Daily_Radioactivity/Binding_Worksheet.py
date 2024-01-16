89df44af2d29f94828ae8521d8194f9d1a23d687
Talia-Albert1 <Talia.Albert@unc.edu>
2024-01-16 08:25:54 -0500

    Printout sheet now starts plate 1 with a 4, better mimics layout in counter

--- Python_Scripts/Daily_Radioactivity/Binding_Worksheet.py
+++ Python_Scripts/Daily_Radioactivity/Binding_Worksheet.py
@@ -499,16 +499,16 @@ log_write('Receptor information added')
 for index, receptor in enumerate(receptors):
     if index < PRIM_count and receptor['Binding Type'].lower() == 'prim':
         ws.cell(index + 4, 9, receptor['Plate Name'])
-        ws.cell(index + 4, 10, index + 1)
+        ws.cell(index + 4, 10, index + 4)
         ws.cell(index + 4, 11, receptor['Barcode 0'])
     elif index >= PRIM_count and receptor['Binding Type'].lower() == 'sec':
         sec_index = 3*(index - PRIM_count) + PRIM_count
         ws.cell(sec_index + 4, 9, receptor['Plate Name'])
         ws.cell(sec_index + 5, 9, receptor['Plate Name'])
         ws.cell(sec_index + 6, 9, receptor['Plate Name'])
-        ws.cell(sec_index + 4, 10, sec_index + 1)
-        ws.cell(sec_index + 5, 10, sec_index + 2)
-        ws.cell(sec_index + 6, 10, sec_index + 3)
+        ws.cell(sec_index + 4, 10, sec_index + 4)
+        ws.cell(sec_index + 5, 10, sec_index + 5)
+        ws.cell(sec_index + 6, 10, sec_index + 6)
         ws.cell(sec_index + 4, 11, receptor['Barcode 0'])
         ws.cell(sec_index + 5, 11, receptor['Barcode 1'])
         ws.cell(sec_index + 6, 11, receptor['Barcode 2'])
