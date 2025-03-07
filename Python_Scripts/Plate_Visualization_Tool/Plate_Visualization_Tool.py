98a1a9ca5605dee318b8b56d0fd926b736b2f380
Talia-Albert1 <Talia.Albert@unc.edu>
2025-03-07 11:02:31 -0500

    Significant headway into completing this

--- Python_Scripts/Plate_Visualization_Tool/Plate_Visualization_Tool.py
+++ Python_Scripts/Plate_Visualization_Tool/Plate_Visualization_Tool.py
@@ -9,6 +9,7 @@ import sys
 import datetime
 import logging
 import shutil
+import pandas as pd
 
 import openpyxl
 from openpyxl.styles import Border, Side, PatternFill, Font, Alignment
@@ -33,10 +34,59 @@ formatted_date = datetime.date.today().strftime('%Y%m%d')
 log_file_name = f'{formatted_date}_Plate_Visualization_Tool_log.log'
 utils.setup_logging(archive_dir, log_filename=log_file_name)
 
+
 # Select Verso Plate Map
-verso_message = "Select the Plate Map file from the Verso (should end in \".txt\")"
-verso_plate_map = utils.select_file_from_input_dir(input_dir, message = verso_message)
+verso_num_files = int(input("Enter the number of Verso Plate Map files: "))
+verso_plate_map_df = []
+for i in range(verso_num_files):
+    verso_message = f"Select the {i + 1} Plate Map file from the Verso (should end in \".txt\")"
+    verso_plate_map_dir = utils.select_file_from_input_dir(input_dir, message = verso_message)
+    verso_plate_map_file_name = os.path.basename(verso_plate_map_dir)
+    try:
+        # Read the file as a DataFrame and add a 'Source' column
+        df = pd.read_csv(verso_plate_map_dir, sep='\t')
+        df['Source_File'] = f'{verso_plate_map_file_name}'
+        verso_plate_map_df.append(df)
+    except Exception as e:
+        print(f"Error reading file {i + 1}: {e}")
+
+
+# Convert Barcode as Float64 to strings without a ".0"
+for i in  range(verso_num_files):
+    verso_plate_map_df[i]['Barcode'] = verso_plate_map_df[i]['Barcode'].fillna(-1).astype('Int64').astype(str)  # Convert int to string
+    verso_plate_map_df[i] = verso_plate_map_df[i][verso_plate_map_df[i]['Barcode'] != '-1']
+
+
 
 # Select Worklist file
-worklist_message = "Select the worklist associated with this plate map file (should end in \".csv\")"
-worklist_file = utils.select_file_from_input_dir(input_dir, message = worklist_message)
+worklist_num_files = int(input("Enter the number of Worklist files: "))
+worklist_data_frames = []
+for i in range(worklist_num_files):
+    worklist_message = "Select the worklist associated with this plate map file (should end in \".csv\")"
+    worklist_file = utils.select_file_from_input_dir(input_dir, message = worklist_message)
+    try:
+        # Read the file as a DataFrame
+        df = pd.read_csv(worklist_file, sep=',')
+        worklist_data_frames.append(df)
+    except Exception as e:
+        print(f"Error reading file {i + 1}: {e}")
+
+
+
+
+
+
+
+# Create Reference map for blank plates
+plate_rows = ['A','B','C','D','E','F','G','H']
+plate_columns = ['1','2','3','4','5','6','7','8','9','10','11','12']
+row_column_list = []
+well_number = 0
+for row in plate_rows:
+    for column in plate_columns:
+        well_number += 1
+        well_id = str(row) + str(column)
+        row_column_list.append([well_id, well_number])
+
+
+# https://stackoverflow.com/questions/15370432/writing-multi-line-strings-into-cells-using-openpyxl
